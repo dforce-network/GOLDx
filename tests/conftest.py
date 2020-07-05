7262bd0b49ceda6db68c120860e22a54c6810403
Miku <Miku@dforce.network>
2020-07-05 23:31:18 +0800

    U Update test cases.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,25 @@
 from brownie import rpc
 
 import pytest
+from decimal import *
+import math
+
+# fix decimal
+getcontext().prec = 72
 
 mint_selector = 0x40c10f19
 burn_selector = 0x9dc29fac
 transfer_selector = 0xa9059cbb
 transferFrom_selector = 0x23b872dd
 function_fee = 0.001e18
+allocate_amount = 50000000e18
+BASE = Decimal(10 ** 18)
+zero_address = '0x0000000000000000000000000000000000000000'
 
 
 @pytest.fixture(scope="function", autouse=True)
 def isolate(fn_isolation):
     pass
-
 
 @pytest.fixture(scope="module")
 def paxg(PAXGImplementation, accounts):
@@ -27,17 +34,15 @@ def paxg(PAXGImplementation, accounts):
     paxg_contract.setFeeRate(20)
     print(f'after  fee rate is {paxg_contract.feeRate()}\n')
 
-    paxg_contract.allocateTo(accounts[0], 5000e18)
-    paxg_contract.allocateTo(accounts[1], 4000e18)
-    paxg_contract.allocateTo(accounts[2], 3000e18)
-    paxg_contract.allocateTo(accounts[3], 2000e18)
+    for i in range(0, 6):
+        paxg_contract.allocateTo(accounts[i], allocate_amount)
 
     return paxg_contract
 
 @pytest.fixture(scope="module")
 def new_paxg(PAXGImplementation, accounts):
     print(f'\n-------- deploy new pax gold contract --------')
-    paxg_contract = accounts[0].deploy(PAXGImplementation, 6)
+    paxg_contract = accounts[0].deploy(PAXGImplementation, 18)
     print(f'new pax gold contract state is: {paxg_contract.paused()}')
     paxg_contract.unpause()
     print(f'new pax gold contract state is: {paxg_contract.paused()}\n')
@@ -46,10 +51,25 @@ def new_paxg(PAXGImplementation, accounts):
     paxg_contract.setFeeRate(20)
     print(f'after  fee rate is {paxg_contract.feeRate()}\n')
 
-    paxg_contract.allocateTo(accounts[0], 5000e18)
-    paxg_contract.allocateTo(accounts[1], 4000e18)
-    paxg_contract.allocateTo(accounts[2], 3000e18)
-    paxg_contract.allocateTo(accounts[3], 2000e18)
+    for i in range(0, 6):
+        paxg_contract.allocateTo(accounts[i], allocate_amount)
+
+    return paxg_contract
+
+@pytest.fixture(scope="module")
+def new_paxg_6(PAXGImplementation, accounts):
+    print(f'\n-------- deploy new pax gold contract --------')
+    paxg_contract = accounts[0].deploy(PAXGImplementation, 6)
+    print(f'new pax gold contract state is: {paxg_contract.paused()}')
+    paxg_contract.unpause()
+    print(f'new pax gold contract state is: {paxg_contract.paused()}\n')
+
+    # print(f'before fee rate is {paxg_contract.feeRate()}')
+    # paxg_contract.setFeeRate(20)
+    # print(f'after  fee rate is {paxg_contract.feeRate()}\n')
+
+    for i in range(0, 6):
+        paxg_contract.allocateTo(accounts[i], allocate_amount)
 
     return paxg_contract
 
@@ -59,11 +79,11 @@ def goldx(GOLDx, paxg, accounts):
     glodx_contract = accounts[0].deploy(GOLDx, "wrapped pax gold", "goldx", paxg.address)
 
     print(f'before, min minting amount is: {glodx_contract.minMintAmount()}')
-    glodx_contract.setMinMintAmount(0.001e18)
+    glodx_contract.setMinMintAmount(0.0001e18)
     print(f'after,  min minting amount is: {glodx_contract.minMintAmount()}\n')
 
     print(f'before, min burning amount is: {glodx_contract.minBurnAmount()}')
-    glodx_contract.setMinBurnAmount(0.001e18)
+    glodx_contract.setMinBurnAmount(0.0001e18)
     print(f'after,  min burning amount is: {glodx_contract.minBurnAmount()}\n')
 
     print(f'before, fee of mint is {glodx_contract.fee(mint_selector)}')
