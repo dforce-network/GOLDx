28dbe0c213382767c84ccfa1034a2d6e5aa9a34c
gaoyang <gy@gmail.com>
2020-07-14 16:58:05 +0800

    combine goldx website and DAPP

--- web-dapp/.umirc.ts
+++ web-dapp/.umirc.ts
@@ -4,11 +4,13 @@ export default defineConfig({
     nodeModulesTransform: {
         type: 'none',
     },
-    routes: [
-        { path: '/user', title: 'user', component: '@/pages/user' },
-        { path: '/', title: 'dForce GOLDx', exact: false, component: '@/pages/index' },
-    ],
+    // routes: [
+    //     { path: '/user', title: 'user', component: '@/pages/user' },
+    //     { path: '/', title: 'dForce GOLDx', exact: false, component: '@/pages/index' },
+    // ],
     hash: true,
     title: 'dForce GOLDx',
     favicon: '/favicon.ico',
+    publicPath: './',
+    history: { type: 'hash' }
 })
