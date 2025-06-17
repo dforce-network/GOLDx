c3ff127144a89482fe54e07dabd355492e5e3593
supermars01 <kings_821113@163.com>
2025-06-17 16:39:36 +0800

    update

--- home/src/index.js
+++ home/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./common/reset.css";
-import "./static/css/main.css";
+import "./static/css/main.scss";
 import "./icons";
 
 ReactDOM.render(<App />, document.getElementById("root"));
